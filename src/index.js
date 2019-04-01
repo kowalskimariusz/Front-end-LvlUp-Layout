ce7830fac47828692156a65447e9792ca4745d05
kowalskimariusz <48931158+kowalskimariusz@users.noreply.github.com>
2019-04-01 23:49:42 +0200

    Second Commit
    Zaimplementowanie layout-u. Dodanie wsparcia SASS.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import defaultMember from "./script.js";
 import './styles/style.css';
+import './styles/style.scss';
 
 defaultMember();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 var path = require('path');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [new HTMLWebpackPlugin()],
-    mode: 'development',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html",
+            title: "Hello there!",
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output; optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
     module: {
         rules: [
             {
@@ -24,6 +36,17 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    isProduction
+                    ? MiniCssExtractPlugin.loader
+                    : { loader: 'style-loader', options: { sourceMap: true } },
+                    { loader: 'css-loader', options: { sourceMap: isProduction } },
+                    { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+                    { loader: 'sass-loader', options: { sourceMap: isProduction } }
+                ]
             }
         ]
     }
