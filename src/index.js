37aa1cc5099692d2d081f0924fe1444f99bfa775
kowalskimariusz <48931158+kowalskimariusz@users.noreply.github.com>
2019-04-02 06:19:30 +0200

    Krok w strone RWD #1
    Pierwszy krok w strone pełnego RWD.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import defaultMember from "./script.js";
+//import defaultMember from "./script.js";
 import './styles/style.css';
 import './styles/style.scss';
 
-defaultMember();
+//defaultMember();
+
+const categories = document.querySelectorAll(".drinks__list>li");
+
+categories.forEach(
+    function(e){
+        e.addEventListener("click",function(ev){
+            categories.forEach(function(el){
+                el.className="drinks__list__drink";
+            });
+            e.className="drinks__list__drink--active";
+        });
+    }
+);
